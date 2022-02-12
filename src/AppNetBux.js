aab1ee2f2e9b68e7f3df50a7faa7d7a2105370b9
ehhwhat <ehhwhat@gmail.com>
2022-02-12 07:26:09 +0000

    Hiding the header and footer for netBux and adding animation

--- src/AppNetBux.js
+++ src/AppNetBux.js
@@ -1,10 +1,14 @@
 import './App.scss';
+import HideHeader from './components/HideHeader'
+import HideFooter from './components/HideFooter'
 import DataBooksFolio from "./assets/data/DataBooksFolio";
 import Books from "./components/Book";
 
 function AppNetBux() {
     return (
         <div className="App">
+            <HideHeader />
+            <HideFooter />
             <main className={'bg-dark'}>
                 <div className={'container-fluid'}>
                     <div className={'row'}>
